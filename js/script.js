7168c4c9085dcfb313aefa3a262448ec946667b7
danirando <danielerando@tiscali.it>
2025-04-11 17:37:48 +0200

    add result

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ for (i = 0; i < 5; i++) {
   randomNumbers.push(generateRandomNumber(1, 50));
 }
 console.log(randomNumbers);
+const instructionEl = document.getElementById("instructions");
 
 const numberOneEl = document.getElementById("numberOne");
 const numberTwoEl = document.getElementById("numberTwo");
@@ -21,6 +22,14 @@ numberTwoEl.innerText = randomNumbers[1];
 numberThreeEl.innerText = randomNumbers[2];
 numberFourEl.innerText = randomNumbers[3];
 numberFiveEl.innerText = randomNumbers[4];
+
+const numberOneGuessEl = document.getElementById("NumberGuessOne");
+const numberTwoGuessEl = document.getElementById("NumberGuessTwo");
+const numberThreeGuessEl = document.getElementById("NumberGuessThree");
+const numberFourGuessEl = document.getElementById("NumberGuessFour");
+const numberFiveGuessEl = document.getElementById("NumberGuessFive");
+
+const messageEl = document.getElementById("message");
 
 function minusOne() {
   if (countDownNumber >= 0) {
@@ -36,6 +45,31 @@ function minusOne() {
       numbersListEl.classList.add("d-none");
       const formEl = document.getElementById("answers-form");
       formEl.classList.remove("d-none");
+
+      let numbersGuesses = [];
+      formEl.addEventListener("submit", function (event) {
+        event.preventDefault();
+        numbersGuesses.push(
+          parseInt(numberOneGuessEl.value),
+          parseInt(numberTwoGuessEl.value),
+          parseInt(numberThreeGuessEl.value),
+          parseInt(numberFourGuessEl.value),
+          parseInt(numberFiveGuessEl.value)
+        );
+        console.log(numbersGuesses);
+
+        const numeriComuni = randomNumbers.filter((num) =>
+          numbersGuesses.includes(num)
+        );
+        console.log(numeriComuni); // Output: [3, 10]
+        console.log(`Numeri in comune: ${numeriComuni.length}`);
+
+        if (numeriComuni.length === 0) {
+          messageEl.innerText = `Non hai ricordato nessun numero`;
+        } else {
+          messageEl.innerText = `Hai ricordato ${numeriComuni.length} numeri: ${numeriComuni}`;
+        }
+      });
     }
     const countDownEl = document.getElementById("countdown");
     countDownEl.innerHTML = countDownNumber;
@@ -45,5 +79,3 @@ function minusOne() {
 const countDown = setInterval(minusOne, 1000);
 
 console.log(countDownNumber);
-
-const instructionEl = document.getElementById("instructions");
